3b072df624f18b6e2231139b401ac9125713e317
sky-033 <sujal.sky07@gmail.com>
2025-07-11 15:25:28 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,11 @@
+let name="Sujal"
+console.log(name[2])
+console.log(name.toUpperCase())
+console.log(name.charAt(1))
+console.log(name.indexOf('j'));
+let newStr=name.substring(0,3)
+console.log(newStr);
+
+console.log(`My name is ${name}`);
+
+
